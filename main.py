9ef8f487bd197ebb00c8e5d7dbcbf3e0dc6fe579
skvadda1 <156102921+skvadda1@users.noreply.github.com>
2024-11-23 10:04:57 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+class quadrilateral:
+    def _init_(self, l, w):
+        self.length = l
+        self.width = w
+def printSquare(self):
+    print(self.length, self.width)
+
+num1 = int(input("What is the length"))
+num2 = int(input("What is the width"))
+
+x = num1 * num2
+print(x)
